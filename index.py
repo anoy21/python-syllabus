df4d8f8dd0cb1618f94584a6a8dd52eaecd0e9ac
anoy21 <132799619+anoy21@users.noreply.github.com>
2025-10-08 12:02:13 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,33 @@
+# EX 2
+    
+# salary = int(input("Enter your salary: "))
+
+# if salary <= 10000:
+#     ptax = salary * 0.9
+# elif 10001 <= salary <= 50000:
+#     ptax = 0.9 * 10000 + (salary - 10000)   * 0.8
+# else:
+#     ptax = 0.9 * 10000 + 0.8 * 40000 + (salary - 50000) * 0.6
+
+# print(ptax)
+
+
+# EX 3
+
+# nums = [2**i for i in range(0,8)]
+# for n in reversed(nums):
+#     print(n)
+
+
+# EX 4
+
+# a = 4
+# b = 6
+# if a < b then a  = a + 2 * b
+# else  a = a**2 +b
+# if a < 20 then b = b + 10
+# else b = b + 5 * a
+# print(a,b)
+
+
+
